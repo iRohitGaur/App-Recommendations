5e1fdb93af4867e39f4073b6e6f1a9646d50f447
Rohit Gaur <knotjinx@gmail.com>
2020-12-11 13:34:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { recommendations } from "./recommendations";
+
+let categories = Object.keys(recommendations);
+
+export default function App() {
+  const [category, setCategory] = useState("top");
+
+  function categoryClickHandler(name) {
+    console.log(name);
+    setCategory(name);
+  }
+
+  function OneCategory(props) {
+    var selectedCat = "";
+    if (props.name === category) {
+      selectedCat = "cat-one selected";
+    } else {
+      selectedCat = "cat-one";
+    }
+
+    return (
+      <div
+        className={selectedCat}
+        onClick={() => categoryClickHandler(props.name)}
+      >
+        {props.name}
+      </div>
+    );
+  }
+
+  function OneRec(props) {
+    return (
+      <div className="rec-one">
+        <div className="rec-one-img-holder">
+          <img
+            className="rec-one-img"
+            src={props.rec.poster}
+            alt={props.rec.name}
+          />
+        </div>
+        <div className="rec-one-details">
+          <div className="name-rating">
+            <div className="name">{props.rec.name}</div>
+            <div className="rating">Rating: {props.rec.rating}</div>
+          </div>
+          <div className="description">{props.rec.description}</div>
+          <div className="links">
+            <a className="link" href={props.rec.web}>
+              Web
+            </a>
+            <a className="link" href={props.rec.android}>
+              Android
+            </a>
+            <a className="link" href={props.rec.ios}>
+              iOS
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function AppRecommendation(props) {
+    return (
+      <div className="rec-all">
+        {recommendations[props.cat].map((item) => (
+          <OneRec key={item.name} rec={item} />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h2 className="header-title">App Recommendations</h2>
+        <h3 className="header-subtitle">collection of best apps</h3>
+        <div className="cat-all">
+          {categories.map((item) => (
+            <OneCategory key={item} name={item} />
+          ))}
+        </div>
+      </div>
+      <AppRecommendation key={category} cat={category} />
+
+      <div className="footer">
+        Made by <a href="https://rohit.xyz">Rohit Gaur</a> with{" "}
+        <i className="fab fa-react"></i> and <i className="fa fa-heart"></i>
+      </div>
+    </div>
+  );
+}
---
+++ src/recommendations.js
@@ -0,0 +1,336 @@
+export let recommendations = {
+  top: [
+    {
+      name: "Brave",
+      description:
+        "Brave is a free and open-source web browser based on the Chromium web browser. It blocks ads and website trackers.",
+      poster:
+        "https://play-lh.googleusercontent.com/I1foi2Irrv7tW9ee9kgP0wfnMzaVb6y17muvpKsFcUrKYsDlmCyWuTRh5m93KJZ24dY=s360-rw",
+      web: "https://brave.com/download/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.brave.browser&hl=en",
+      ios:
+        "https://apps.apple.com/us/app/brave-private-web-browser/id1052879175?mt=8&ign-mpt=uo%3D4",
+      rating: "9/10"
+    },
+    {
+      name: "Telegram",
+      description:
+        "Telegram is a cross-platform cloud-based instant messaging, video calling, and VoIP service. It provides end-to-end encrypted calls.",
+      poster:
+        "https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q=s360-rw",
+      web: "https://desktop.telegram.org/",
+      android:
+        "https://play.google.com/store/apps/details?id=org.telegram.messenger",
+      ios: "https://apps.apple.com/app/telegram-messenger/id686449807",
+      rating: "9.5/10"
+    },
+    {
+      name: "Twitter",
+      description:
+        'Twitter is a microblogging and social networking service on which users post and interact with messages known as "tweets".',
+      poster:
+        "https://play-lh.googleusercontent.com/wIf3HtczQDjHzHuu7vezhqNs0zXAG85F7VmP7nhsTxO3OHegrVXlqIh_DWBYi86FTIGk=s360-rw",
+      web: "https://twitter.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.twitter.android",
+      ios: "https://apps.apple.com/us/app/twitter/id333903271",
+      rating: "9/10"
+    },
+    {
+      name: "Swiggy",
+      description:
+        "Swiggy is India's largest online food ordering and delivery platform. It expanded into general product deliveries under the name Swiggy Stores.",
+      poster:
+        "https://play-lh.googleusercontent.com/A8jF58KO1y2uHPBUaaHbs9zSvPHoS1FrMdrg8jooV9ftDidkOhnKNWacfPhjKae1IA=s360-rw",
+      web: "https://www.swiggy.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=in.swiggy.android",
+      ios:
+        "https://apps.apple.com/in/app/swiggy-food-order-delivery/id989540920",
+      rating: "9/10"
+    },
+    {
+      name: "Google Drive",
+      description:
+        "It allows users to store files on their servers, synchronize files across devices, and share files. Users get 15GB free.",
+      poster:
+        "https://play-lh.googleusercontent.com/t-juVwXA8lDAk8uQ2L6d6K83jpgQoqmK1icB_l9yvhIAQ2QT_1XbRwg5IpY08906qEw=s360-rw",
+      web: "http://google.com/intl/en_in/drive/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.google.android.apps.docs",
+      ios: "https://apps.apple.com/us/app/google-drive/id507874739",
+      rating: "9.5/10"
+    }
+  ],
+
+  browser: [
+    {
+      name: "Brave",
+      description:
+        "Brave is a free and open-source web browser based on the Chromium web browser. It blocks ads and website trackers.",
+      poster:
+        "https://play-lh.googleusercontent.com/I1foi2Irrv7tW9ee9kgP0wfnMzaVb6y17muvpKsFcUrKYsDlmCyWuTRh5m93KJZ24dY=s360-rw",
+      web: "https://brave.com/download/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.brave.browser&hl=en",
+      ios:
+        "https://apps.apple.com/us/app/brave-private-web-browser/id1052879175?mt=8&ign-mpt=uo%3D4",
+      rating: "9/10"
+    },
+    {
+      name: "Firefox",
+      description:
+        "Mozilla Firefox is a free and open-source web browser. It uses the Gecko layout engine to render web pages.",
+      poster:
+        "https://play-lh.googleusercontent.com/zqsuwFUBwKRcGOSBinKQCL3JgfvOW49vJphq0ZF32aDgfqmuDyl-fEpx4Lxm4pRr7A=s360-rw",
+      web: "https://www.mozilla.org/en-US/firefox/new/",
+      android:
+        "https://play.google.com/store/apps/details?id=org.mozilla.firefox",
+      ios:
+        "https://itunes.apple.com/us/app/firefox-private-safe-browser/id989804926",
+      rating: "8/10"
+    },
+    {
+      name: "Edge",
+      description:
+        "Microsoft Edge is a cross-platform web browser. It includes integration with Cortana and has extensions hosted on the Microsoft Store.",
+      poster:
+        "https://play-lh.googleusercontent.com/VYvJqGnrQiKkbbyLyMeiL-GM3go4tBIA64uVEGQazLXD4p_M3F45kHyt42o_6d5VXA=s360-rw",
+      web: "https://www.microsoft.com/en-us/edge",
+      android:
+        "https://play.google.com/store/apps/details?id=com.microsoft.emmx",
+      ios: "https://apps.apple.com/app/apple-store/id1288723196?mt=8",
+      rating: "7/10"
+    },
+    {
+      name: "Chrome",
+      description:
+        "Google Chrome is a cross-platform web browser. As of November 2020, StatCounter estimates that Chrome has a 70% worldwide browser market share.",
+      poster:
+        "https://play-lh.googleusercontent.com/KwUBNPbMTk9jDXYS2AeX3illtVRTkrKVh5xR1Mg4WHd0CG2tV4mrh1z3kXi5z_warlk=s360-rw",
+      web: "https://www.google.com/chrome/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.android.chrome&pcampaignid=websitedialog",
+      ios: "https://itunes.apple.com/us/app/chrome/id535886823",
+      rating: "7/10"
+    }
+  ],
+
+  communication: [
+    {
+      name: "Telegram",
+      description:
+        "Telegram is a cross-platform cloud-based instant messaging, video calling, and VoIP service. It provides end-to-end encrypted calls.",
+      poster:
+        "https://play-lh.googleusercontent.com/ZU9cSsyIJZo6Oy7HTHiEPwZg0m2Crep-d5ZrfajqtsH-qgUXSqKpNA2FpPDTn-7qA5Q=s360-rw",
+      web: "https://desktop.telegram.org/",
+      android:
+        "https://play.google.com/store/apps/details?id=org.telegram.messenger",
+      ios: "https://apps.apple.com/app/telegram-messenger/id686449807",
+      rating: "9.5/10"
+    },
+    {
+      name: "WhatsApp",
+      description:
+        "WhatsApp Messenger is a cross-platform messaging and VoIP service. It allows users to send text and voice messages, make voice and video calls.",
+      poster:
+        "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s360-rw",
+      web: "https://www.whatsapp.com/download/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.whatsapp&hl=en_IN&gl=US",
+      ios: "https://apps.apple.com/us/app/whatsapp-messenger/id310633997",
+      rating: "9/10"
+    },
+    {
+      name: "Facebook",
+      description:
+        "Facebook Messenger is a messaging app and platform. Users can send messages and exchange photos, videos, stickers, audio, and files.",
+      poster:
+        "https://play-lh.googleusercontent.com/ldcQMpP7OaVmglCF6kGas9cY_K0PsJzSSosx2saw9KF1m3RHaEXpH_9mwBWaYnkmctk=s360-rw",
+      web: "https://www.messenger.com/desktop",
+      android:
+        "https://play.google.com/store/apps/details?id=com.facebook.orca&hl=en_IN&gl=US",
+      ios: "https://apps.apple.com/us/app/messenger/id454638411",
+      rating: "8/10"
+    },
+    {
+      name: "Skype",
+      description:
+        "Skype is a telecommunications application that specializes in providing video chat and voice calls between devices over the Internet.",
+      poster:
+        "https://play-lh.googleusercontent.com/Tqgy9wddo_e_IegqVDAiYfPcWg0iXESll_DvU2yDfdJQCj-gzWcGOnO9tb_WY-dEUuY=s360-rw",
+      web: "https://www.skype.com/en/get-skype/",
+      android: "https://play.google.com/store/apps/details?id=com.skype.m2",
+      ios: "https://apps.apple.com/app/apple-store/id304878510",
+      rating: "8/10"
+    }
+  ],
+
+  social: [
+    {
+      name: "Twitter",
+      description:
+        'Twitter is a microblogging and social networking service on which users post and interact with messages known as "tweets".',
+      poster:
+        "https://play-lh.googleusercontent.com/wIf3HtczQDjHzHuu7vezhqNs0zXAG85F7VmP7nhsTxO3OHegrVXlqIh_DWBYi86FTIGk=s360-rw",
+      web: "https://twitter.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.twitter.android",
+      ios: "https://apps.apple.com/us/app/twitter/id333903271",
+      rating: "9/10"
+    },
+    {
+      name: "LinkedIn",
+      description:
+        "LinkedIn is a business and employment-oriented online service. It is mainly used for professional networking.",
+      poster:
+        "https://play-lh.googleusercontent.com/fqYJHtyzZzA4vacRzeJoB93QNvA5-mvR-8UB5oVLxdYDSTpfLp_KgYD4IqVGJUgFEJo=s360-rw",
+      web: "https://www.linkedin.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.linkedin.android",
+      ios: "https://apps.apple.com/us/app/id288429040",
+      rating: "9/10"
+    },
+    {
+      name: "Instagram",
+      description:
+        "Instagram is a photo and video sharing social networking service. The app allows users to upload media that can be edited with filters.",
+      poster:
+        "https://play-lh.googleusercontent.com/h9jWMwqb-h9hjP4THqrJ50eIwPekjv7QPmTpA85gFQ10PjV02CoGAcYLLptqd19Sa1iJ=s360-rw",
+      web: "https://www.instagram.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.instagram.android",
+      ios: "https://apps.apple.com/us/app/instagram/id389801252",
+      rating: "9/10"
+    },
+    {
+      name: "YouTube",
+      description:
+        "YouTube is an online video-sharing platform. It allows users to upload, view, rate, share, comment on videos, and subscribe to other users.",
+      poster:
+        "https://play-lh.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc=s360-rw",
+      web: "https://www.youtube.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.google.android.youtube",
+      ios:
+        "https://apps.apple.com/in/app/youtube-watch-listen-stream/id544007664",
+      rating: "9/10"
+    },
+    {
+      name: "Facebook",
+      description:
+        "Facebook is an online social media and social networking service. Users can post text, photos and multimedia with other users.",
+      poster:
+        "https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=s360-rw",
+      web: "https://www.facebook.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.facebook.katana",
+      ios: "https://apps.apple.com/in/app/facebook/id284882215",
+      rating: "8/10"
+    }
+  ],
+
+  food: [
+    {
+      name: "Swiggy",
+      description:
+        "Swiggy is India's largest online food ordering and delivery platform. It expanded into general product deliveries under the name Swiggy Stores.",
+      poster:
+        "https://play-lh.googleusercontent.com/A8jF58KO1y2uHPBUaaHbs9zSvPHoS1FrMdrg8jooV9ftDidkOhnKNWacfPhjKae1IA=s360-rw",
+      web: "https://www.swiggy.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=in.swiggy.android",
+      ios:
+        "https://apps.apple.com/in/app/swiggy-food-order-delivery/id989540920",
+      rating: "9/10"
+    },
+    {
+      name: "Zomato",
+      description:
+        "Zomato is an Indian restaurant aggregator and food delivery start-up. It provides information, menus, user-reviews and food delivery options.",
+      poster:
+        "https://play-lh.googleusercontent.com/s7sXk7xzigkP4pQlHsJR-tqL-z4QUsdsAZYJB4b-goaSX1toOo-NcHaibo0gOKWbdMcG=s360-rw",
+      web: "https://www.zomato.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.application.zomato",
+      ios: "https://apps.apple.com/in/app/zomato-food-restaurants/id434613896",
+      rating: "9/10"
+    },
+    {
+      name: "UberEats",
+      description:
+        "Uber Eats is an online food ordering and delivery platform. Users can read menus, reviews and ratings, and order food from participating restaurants.",
+      poster:
+        "https://play-lh.googleusercontent.com/MMBG4AZmpMhSfhF5k7QnFmhvFbaF5ZC_BtEOIKRt9TIkUZjul2lWwPZV75PwTfoSm23-jgMxkroRGA-vkDg=s360-rw",
+      web: "https://www.ubereats.com/",
+      android: "https://play.google.com/store/apps/details?id=com.ubercab.eats",
+      ios: "https://apps.apple.com/us/app/uber-eats-food-delivery/id1058959277",
+      rating: "8/10"
+    },
+    {
+      name: "Faasos",
+      description:
+        'Faasos is an Indian "food on demand" service. It operates in Dubai and 34 of the largest cities in India.',
+      poster:
+        "https://play-lh.googleusercontent.com/CKBAqs2TuZZTobo5pCK-i1g9EvyKuyQfqqssfkl0CVHiPFos0FcFH70SQoNDYYqcOIs=s360-rw",
+      web: "https://www.faasos.com/",
+      android: "https://play.google.com/store/apps/details?id=com.done.faasos",
+      ios: "https://apps.apple.com/in/app/eatsure-food-delivery/id808796222",
+      rating: "8/10"
+    }
+  ],
+
+  storage: [
+    {
+      name: "Google Drive",
+      description:
+        "It allows users to store files on their servers, synchronize files across devices, and share files. Users get 15GB free.",
+      poster:
+        "https://play-lh.googleusercontent.com/t-juVwXA8lDAk8uQ2L6d6K83jpgQoqmK1icB_l9yvhIAQ2QT_1XbRwg5IpY08906qEw=s360-rw",
+      web: "http://google.com/intl/en_in/drive/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.google.android.apps.docs",
+      ios: "https://apps.apple.com/us/app/google-drive/id507874739",
+      rating: "9.5/10"
+    },
+    {
+      name: "Microsoft OneDrive",
+      description:
+        "Microsoft OneDrive is a file hosting and synchronization service. OneDrive offers 5 GB of storage space free of charge.",
+      poster:
+        "https://play-lh.googleusercontent.com/4iLRlu-E5hbgoHCb5dkR8wUbjEHFWHfXHkYKexE9hGVaISUGuYzwnFsSvKLgQWTi3cg=s360-rw",
+      web:
+        "https://www.microsoft.com/en-in/microsoft-365/onedrive/online-cloud-storage",
+      android:
+        "https://play.google.com/store/apps/details?id=com.microsoft.skydrive",
+      ios: "https://apps.apple.com/us/app/microsoft-onedrive/id477537958",
+      rating: "9/10"
+    },
+    {
+      name: "Dropbox",
+      description:
+        "Dropbox is a file hosting and synchronization service. Individual accounts get 2GB for free.",
+      poster:
+        "https://play-lh.googleusercontent.com/Tcxceg1D7JGYGmxcrnyjDY8EafLg9VQPW2dqu3kHczfvERGd0h6vgS6KaYPKsW4ZE7z6=s360-rw",
+      web: "https://www.dropbox.com/",
+      android:
+        "https://play.google.com/store/apps/details?id=com.dropbox.android",
+      ios:
+        "https://apps.apple.com/us/app/dropbox-backup-sync-share/id327630330",
+      rating: "9/10"
+    },
+    {
+      name: "Box",
+      description:
+        "Box is a cloud computing business which provides file sharing and collaborating. Personal accounts get 10GB of free storage.",
+      poster:
+        "https://play-lh.googleusercontent.com/bYVafDlBEUJf-_agMoV97k336w4VSGMyIVNq5aJWMYlpyDq9y6hmTuso5cynuBDP8cI=s360-rw",
+      web: "https://www.box.com/en-in/cloud-storage",
+      android: "https://play.google.com/store/apps/details?id=com.box.android",
+      ios:
+        "https://apps.apple.com/us/app/box-cloud-content-management/id290853822",
+      rating: "8/10"
+    }
+  ]
+};
